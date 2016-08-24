d0cc85ff4586ea48d13ed3c0ba5b4636eab5aea5
pnsolution <contact@pnsolution.com>
2016-08-24 16:01:52 +0700

    map

--- Saphaipae/RoutePage.xaml.cs
+++ Saphaipae/RoutePage.xaml.cs
@@ -12,7 +12,19 @@ namespace Saphaipae
 		public RoutePage ()
 		{
 			InitializeComponent ();
-		}
+            var map = new Map(
+            MapSpan.FromCenterAndRadius(
+                    new Position(37, -122), Distance.FromMiles(0.3)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
+        }
 	}
 }
 
