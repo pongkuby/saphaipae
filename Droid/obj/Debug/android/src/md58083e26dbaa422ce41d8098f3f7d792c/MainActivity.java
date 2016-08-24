b573859812b9c56db1aef6f901f14595025c9cb5
pnsolution <contact@pnsolution.com>
2016-08-24 15:53:20 +0700

    เพิ่ม maps

---
+++ Droid/obj/Debug/android/src/md58083e26dbaa422ce41d8098f3f7d792c/MainActivity.java
@@ -0,0 +1,47 @@
+package md58083e26dbaa422ce41d8098f3f7d792c;
+
+
+public class MainActivity
+	extends md5b60ffeb829f638581ab2bb9b1a7f4f3f.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Saphaipae.Droid.MainActivity, Saphaipae.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Saphaipae.Droid.MainActivity, Saphaipae.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
--- Saphaipae/obj/Debug/Saphaipae.RoutePage.xaml.g.cs
+++ Saphaipae/obj/Debug/Saphaipae.RoutePage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace Saphaipae {
     public partial class RoutePage : global::Xamarin.Forms.ContentPage {
         
         [System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Maps.Map MyMap;
+        
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             this.LoadFromXaml(typeof(RoutePage));
+            MyMap = this.FindByName<global::Xamarin.Forms.Maps.Map>("MyMap");
         }
     }
 }
